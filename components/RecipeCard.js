00d05fad3230a0786866ed5237520ce8ebebfe0a
CGabriel22 <carlosgabrieldev@gmail.com>
2021-07-14 12:18:03 -0300

    first commit

---
+++ components/RecipeCard.js
@@ -0,0 +1,9 @@
+export default function RecipeCard({ recipe }) {
+    return (
+        <div>
+            {recipe.fields.title}
+        </div>
+    )
+}
+// rfc - cria o componente
+// parei no minuto 6:00 do video #4 next.js & Contentful tutorial
